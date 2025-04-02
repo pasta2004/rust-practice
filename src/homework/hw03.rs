0171b8842b75d93cfeabbf097e16ca2dc4f18860
pasta2004 <cornpuff2004@gmail.com>
2025-04-02 16:24:15 +0300

    Update hw03.rs

--- src/homework/hw03.rs
+++ src/homework/hw03.rs
@@ -1,30 +1,26 @@
-const WIDTH: usize = 10;
-const HEIGHT: usize = 6;
+const W: u32 = 20;
+const H: u32 = 10;
 
-fn main() {
+fn envelope(width: u32, height: u32) {
+    let k: f32 = width as f32 / height as f32;
     let mut output = String::new();
 
-    for y in 0..HEIGHT {
-        for x in 0..WIDTH {
-            if x == 0 || x == WIDTH - 1 {
-                output.push('*');
-            } else if y == 0 || y == HEIGHT - 1 {
-                output.push('*');
-            } else if x == y || x == WIDTH - y - 1 {
-                output.push('*');
-            } else {
-                output.push(' ');
-            }
+    for y in 0..height {
+        for x in 0..width {
+            let is_hor = y == 0 || y == height - 1;
+            let is_ver = x == 0 || x == width - 1;
+            let is_diag = x == (y as f32 * k) as u32;
+            let is_diag2 = x == width - 1 - (y as f32 * k) as u32;
+            let show = is_hor || is_ver || is_diag || is_diag2;
+
+            output.push(if show { '*' } else { ' ' });
         }
         output.push('\n');
     }
-    
-    println!("{}", output);
+
+    print!("{}", output);
 }
 
-/**********
- **      **
- * *    * *
- *  *  *  *
- *   **   *
- **********/
+fn main() {
+    envelope(W, H);
+}
